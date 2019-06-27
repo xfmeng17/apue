492f46e22db6295ef9127ac17590e1ece2576477
xfmeng17 <xfmeng17@foxmail.com>
2019-06-27 23:50:42 +0800

    feat(ch04): finish exercise 4.6
    1. I think I already finish exercise 4.6 by the code implement in
    ch04/filehole/filehole.c.
    2. As I mention in filehole.c's comment, the SEEK_HOLE/SEEK_DATA
    are treat 4096(4K) bytes as one part. Which means I will fill some
    extra '\0' after 'cp' utility described in exercise 4.6.
    
    PS: I think I am doing some great work and enjoy this learning.
    ^_^

--- ch04/filehole/filehole.c
+++ ch04/filehole/filehole.c
@@ -5,7 +5,7 @@
 
 char buf1[] = "abcdexxxxx";
 char buf2[] = "ABCDEXXXXX";
-char buf3[16394];
+char buf3[16384];
 
 int main(int argc, char** argv) {
 	//** create testfile.hold
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 	if (write(fd, buf1, 10) != 10) {
 		err_sys("buf1 write error");
 	}
-	if (lseek(fd, 16384, SEEK_SET) == -1) {
+	if (lseek(fd, 16374, SEEK_SET) == -1) {
 		err_sys("lseek error");
 	}
 	if (write(fd, buf2, 10) != 10) {
@@ -29,22 +29,33 @@ int main(int argc, char** argv) {
 		err_sys("open file.hole error");
 	}
 	ssize_t read_len = -1;
-	if ((read_len = read(fd, buf3, 16394)) < 0) {
+	if ((read_len = read(fd, buf3, 16384)) < 0) {
 		err_sys("buf3 read from file error");
 	}
 	printf("read_len=%ld\n", read_len);
+	close(fd);
 
 	//** write buf3 to file.copy
-	close(fd);
 	if ((fd = creat("file.copy", FILE_MODE)) < 0) {
 		err_sys("creat file.copy error");
 	}
-	if (write(fd, buf3, 16394) != 16394) {
+	if (write(fd, buf3, 16384) != 16384) {
 		err_sys("write buf3 to file.copy error");
 	}
 	close(fd);
 	
-	//** find hole by useing lseek whence SEEK_HOLE
+	/*
+	** In Ununtu18.04, SEEK_HOLE and SEEK_DATA are all
+	** treat 4096(4K) as one part, we can call 'block'
+	** in a file.
+	** 
+	** That is, for exercise 4.6, I cannot completely
+	** "cp" the hole file to and other without any extra
+	** '\0'. There will fill some additional '\0' in the
+	** data block.
+	*/
+
+	//** find hole by lseek whence=SEEK_HOLE
 	if ((fd = open("file.hole", O_RDWR)) < 0) {
 		err_sys("open file.hole error");
 	}
@@ -60,15 +71,53 @@ int main(int argc, char** argv) {
 	off_t next_hole_4097 = lseek(fd, 4097, SEEK_HOLE);
 	printf("next_hole_4097=%ld\n", next_hole_4097);
 
-	off_t next_hole_16381 = lseek(fd, 16381, SEEK_HOLE);
-	printf("next_hole_16381=%ld\n", next_hole_16381);
+	off_t next_hole_16373 = lseek(fd, 16373, SEEK_HOLE);
+	printf("next_hole_16373=%ld\n", next_hole_16373);
 	
-	off_t next_hole_16384 = lseek(fd, 16384, SEEK_HOLE);
-	printf("next_hole_16384=%ld\n", next_hole_16384);
+	off_t next_hole_16374 = lseek(fd, 16374, SEEK_HOLE);
+	printf("next_hole_16374=%ld\n", next_hole_16374);
 	
-	off_t next_hole_16390 = lseek(fd, 16390, SEEK_HOLE);
-	printf("next_hole_16390=%ld\n", next_hole_16390);
+	off_t next_hole_16375 = lseek(fd, 16375, SEEK_HOLE);
+	printf("next_hole_16375=%ld\n", next_hole_16375);
+
+	//** find data by lseek whence=SEEK_DATA
+	off_t next_data_0 = lseek(fd, 0, SEEK_DATA);
+	printf("next_data_0=%ld\n", next_data_0);
 	
+	off_t next_data_9 = lseek(fd, 9, SEEK_DATA);
+	printf("next_data_9=%ld\n", next_data_9);
+
+	off_t next_data_10 = lseek(fd, 10, SEEK_DATA);
+	printf("next_data_10=%ld\n", next_data_10);
+
+	off_t next_data_11 = lseek(fd, 11, SEEK_DATA);
+	printf("next_data_11=%ld\n", next_data_11);
+	
+	off_t next_data_100 = lseek(fd, 100, SEEK_DATA);
+	printf("next_data_100=%ld\n", next_data_100);
+
+	off_t next_data_4100 = lseek(fd, 4100, SEEK_DATA);
+	printf("next_data_4100=%ld\n", next_data_4100);
+
+	off_t next_data_12287 = lseek(fd, 12287, SEEK_DATA);
+	printf("next_data_12287=%ld\n", next_data_12287);
+
+	off_t next_data_12288 = lseek(fd, 12288, SEEK_DATA);
+	printf("next_data_12288=%ld\n", next_data_12288);
+
+	off_t next_data_12289 = lseek(fd, 12289, SEEK_DATA);
+	printf("next_data_12289=%ld\n", next_data_12289);
+
+	off_t next_data_16373 = lseek(fd, 16373, SEEK_DATA);
+	printf("next_data_16373=%ld\n", next_data_16373);
+	
+	off_t next_data_16374 = lseek(fd, 16374, SEEK_DATA);
+	printf("next_data_16374=%ld\n", next_data_16374);
+	
+	off_t next_data_16380 = lseek(fd, 16380, SEEK_DATA);
+	printf("next_data_16380=%ld\n", next_data_16380);
+	close(fd);
+
 	exit(0);
 }
 
