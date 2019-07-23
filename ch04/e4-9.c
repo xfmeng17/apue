3102ab2feeca009f6da4f558bda9e49ec8ce73cb
xfmeng17 <xfmeng17@foxmail.com>
2019-07-23 14:06:32 +0800

    docs(ch04): append e4-9 text solution from Appendix C

--- ch04/e4-9.c
+++ ch04/e4-9.c
@@ -5,13 +5,20 @@
 
 int main(int argc, char** argv) {
 	umask(0);
-	if (creat("foo", RWRWRW) < 0) {
+	if (creat("e4-9.foo", RWRWRW) < 0) {
 		err_sys("create error for foo");
 	}
 	umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-	if (creat("bar", RWRWRW) < 0) {
+	if (creat("e4-9.bar", RWRWRW) < 0) {
 		err_sys("create error for bar");
 	}
 	exit(0);
 }
+
+
+/*
+ * If we try, using either open() or creat(), to create a file that already
+ * exists, the file's access permission bits are not change. We can verify
+ * this by running the program from Figure 4.9.
+ */
 
