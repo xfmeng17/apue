4ec8173ade06f4391f673e2ad6f5c9f3d528ba22
xfmeng17 <xfmeng17@foxmail.com>
2019-07-24 17:32:04 +0800

    feat(ch04): finish exercise 4.7 - 4.9

--- ch04/unlink/test_unlink.c
+++ ch04/unlink/test_unlink.c
@@ -5,9 +5,9 @@ int main(void) {
 	if (open("tempfile", O_RDWR | O_CREAT) < 0) {
 		err_sys("open error");
 	}
-	if (link("tempfile", "tempfile_link1") < 0) {
-		err_sys("link error");
-	}
+	// if (link("tempfile", "tempfile_link1") < 0) {
+	// 	err_sys("link error");
+	// }
 	sleep(15);
 	if (unlink("tempfile") < 0) {
 		err_sys("unlink error");
