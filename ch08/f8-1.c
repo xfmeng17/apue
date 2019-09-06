d3bf5719bc43d27cf2b970aad79f9174facc94ec
xfmeng17 <xfmeng17@foxmail.com>
2019-09-06 10:14:02 +0800

    feat(ch08): add f8-1.c, write function is not buffered

--- ch08/f8-1.c
+++ ch08/f8-1.c
@@ -1,17 +1,18 @@
 #include "apue.h"
 
 int globvar = 6;
-char buf[] = "a write to stdout|\n";
+char buf[] = "[a write to stdout]\n";
 
 int main(void) {
 	int var;
 	pid_t pid;
 
 	var = 88;
-	if (write(STDOUT_FILENO, buf, sizeof(buf) - 2) != sizeof(buf) - 2) {
+	if (write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1) {
 		err_sys("write error");
 	}
-	printf("before fork|");
+	printf("[before fork]");
+	// printf("\n");
 
 	if ((pid = fork()) < 0) {
 		err_sys("fork error");
@@ -21,8 +22,7 @@ int main(void) {
 	} else {
 		sleep(2);
 	}
-
-	printf("pid=%ld, glob=%d, var=%d\n", (long)getpid(), globvar, var);
+	printf("[pid=%ld, glob=%d, var=%d]\n", (long)getpid(), globvar, var);
 	exit(0);
 }
 
