27f0a43df3f0c6b04c3357550b0309a59be72975
xfmeng17 <xfmeng17@foxmail.com>
2019-11-07 12:32:20 +0800

    feat(ch10): update figure 10.15
    1. This figure practice function `sigprocmask()` and `sigpending()`.
    2. When unblock SIGQUIT, the quit signal will send to process only once if we
    send it to process 1 or more times.
    3. After calling `sig_quit()`, the handle of SIGQUIT will be setting to default
    that stop the process and print `Quit(coredump)` to stdout. We should re-signal
    SIGQUIT in `sig_quit()` if we don't want the default behavior.
    4. When we printf message like ">>> the %d second sleep" around code line 48, we
    should use "\n" to flush stdout.

--- ch10/f10-15.c
+++ ch10/f10-15.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 
 static void sig_quit(int);
+static int sig_catch_cnt = 0;
 
 int main(void) {
 	sigset_t newmask, oldmask, pendmask;
@@ -36,21 +37,30 @@ int main(void) {
 	/*
 	 * Restore signal mask which unblocks SIGQUIT
 	 */
+	printf("\nStart SIGQUIT unblocked\n");
 	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0) {
 		err_sys("SIG_SETMASK error");
 	}
-	printf("\nSIGQUIT unblocked\n");
+	printf("\nAfter SIGQUIT unblocked sleeping: \n");
 
 	/* SIGQUIT here will terminate with core file */ 
-	sleep(30);
+	for (int i = 0; i < 30; i++) {
+		printf(">>> the %d second sleep\n", i);
+		sleep(1);
+	}
+	printf("\n");
 	exit(0);
 }
 
 static void sig_quit(int signo) {
-	printf("caught SIGQUIT");
+	printf("\nThe [%d] time caught SIGQUIT in sig_quit()\n", sig_catch_cnt++);
 	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR) {
 		err_sys("can't reset SIGQUIT");
 	}
-
+	if (signal(SIGQUIT, sig_quit) == SIG_ERR) {
+		err_sys("can't catch SIGQUIT");
+	} else {
+		printf("sig SIGQUIT again in sig_quit() !!!\n");
+	}
 }
 
