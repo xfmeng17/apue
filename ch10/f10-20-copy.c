c394d9e741b6e1b3a4df185884f5dfbe3451bcad
xfmeng17 <xfmeng17@foxmail.com>
2019-11-18 00:23:37 +0800

    chore(ch10): update log file

--- ch10/f10-20-copy.c
+++ ch10/f10-20-copy.c
@@ -1,59 +1,43 @@
 #include "apue.h"
-#include <setjmp.h>
-#include <errno.h>
-#include <time.h>
 
+static void sig_int(int);
 static void sig_usr1(int);
-static void sig_alrm(int);
-static sigjmp_buf jmpbuf;
-static volatile sig_atomic_t canjump;
-
-static void my_pr_mask(const char *, sigset_t *);
+static int sleep_cnt = 0;
+static void pr_sleep(const char*, int);
 
 int main(void) {
+	sigset_t newmask, oldmask, waitmask;
+	
+	pr_mask("program start: ");
+
+	if (signal(SIGINT, sig_int) == SIG_ERR) {
+		err_sys("signal(SIGINT) error");
+	}
 	if (signal(SIGUSR1, sig_usr1) == SIG_ERR) {
 		err_sys("signal(SIGUSR1) error");
 	}
-	if (signal(SIGALRM, sig_alrm) == SIG_ERR) {
-		err_sys("signal(SIGALRM) error");
+
+	sigemptyset(&waitmask);
+	sigaddset(&waitmask, SIGUSR1);
+	sigemptyset(&newmask);
+	sigaddset(&newmask, SIGINT);
+
+	//** Block SIGINT and save current signal mask.
+	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0) {
+		err_sys("SIG_BLOCK error");
 	}
+	pr_sleep("bolck SIGINT", 10);
+
+	//** Critical region of code
+	pr_mask("in critical region:");
+
+	//** Pause, allowing all signals except SIGUSR1
+	if (sigsuspend(&waitmask) != -1) {
+		err_sys("sigsuspend error");
+	}
+	pr_sleep("sigsuspend SIGUSR1", 10);
 	
-	my_pr_mask("starting main: ", NULL);
-	
-	if (sigsetjmp(jmpbuf, 1)) {
-		my_pr_mask("ending main: ", NULL);
-	}
-	canjump = 1;
+	pr_mask("after return from sigsuspend: ");
 
-	for (;;) {
-		pause();
-	}
-}
-
-static void sig_usr1(int signo) {
-	time_t starttime;
-
-	/* unexpected signal, ignore */
-	if (canjump == 0) {
-		return;
-	}
-	
-	my_pr_mask("starting sig_usr1: ", NULL);
-
-	alarm(3);
-	starttime = time(NULL);
-	for (;;) {
-		if (time(NULL) > starttime + 5) {
-			break;
-		}
-	}
-
-	my_pr_mask("finisning sig_usr1: ", NULL);
-
-	canjump = 0;
-	/* jump to main, don't return */
-	siglongjmp(jmpbuf, 1);
-}
-
-static void sig_alrm(int signo) {
-	my_pr_mask("in s
+	//** Reset signal mask which unblock SIGINT
+	if (sigprocmask(SIG_SETMASK, &oldmask,
