4bf8e0042f4dd207e472528579bb2f61a82de644
xfmeng17 <xfmeng17@foxmail.com>
2019-11-20 10:51:39 +0800

    feat(ch10): update e10-10.c with printing Calendar time
    The print is vary on time in the second level. Maybe I should print some us level
    log, then may show some miss.

--- ch10/e10-10.c
+++ ch10/e10-10.c
@@ -1,5 +1,7 @@
 /* 2019-11-17 23:59
  * Should setbuf to line then I can see printf in log file.
+ * 2019-11-18
+ * nohup ./e10-10.out > e10-10.log 2>&1 &
  */
 
 #include "apue.h"
@@ -8,7 +10,9 @@
 int main(void) {
 	int cnt = 0;
 	time_t rawtime;
+	time_t lasttime = 0;
 	struct tm *tm_info = NULL;
+
 
 	setbuf(stdout, 0);
 
@@ -20,6 +24,8 @@ int main(void) {
 			time(&rawtime);
 			tm_info = localtime(&rawtime);
 			printf("Current local time and date: %s\n", asctime(tm_info));
+			printf("time_t difference = %ld\n", rawtime - lasttime);
+			lasttime = rawtime;
 		}
 	}
 
