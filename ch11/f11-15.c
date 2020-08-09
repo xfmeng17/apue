5de1ddcfe54bee96243cfbb565da0b0e6676be61
xfmeng17 <xfmeng17@foxmail.com>
2020-08-09 12:59:15 +0800

    2020-08-09 12:59:15

--- ch11/f11-15.c
+++ ch11/f11-15.c
@@ -1,7 +1,13 @@
+#include "apue.h"
+
 #include <pthread.h>
+
+#define EQ_NUM 10
 
 struct msg {
 	struct msg *m_next;
+	time_t m_time;
+	int m_num;
 	//** more stuff here
 };
 
@@ -22,6 +28,9 @@ void process_msg(void) {
 		workq = mp->m_next;
 		pthread_mutex_unlock(&qlock);
 		//** now process the message mp
+		printf("worker=%ld, m_time=%ld, m_num=%d\n",
+			   pthread_self(), mp->m_time, mp->m_num);
+		sleep(1);
 	}
 }
 
@@ -33,6 +42,37 @@ void enqueue_msg(struct msg *mp) {
 	pthread_cond_signal(&qready);
 }
 
+void* thread_process(void* p) {
+	printf("process_msg,n=%d,tid=%ld\n", *((int*)p), pthread_self());
+	process_msg();
+}
+void* thread_enqueue(void* p) {
+	printf("*p=%d\n", *((int*)p));
+	for (int i = 0; i < EQ_NUM; i++) {
+		struct msg *mp = (struct msg*)malloc(sizeof(struct msg));
+		mp->m_next = NULL;
+		mp->m_time = time(NULL);
+		mp->m_num = *((int*)p);
+		enqueue_msg(mp);
+		sleep(1);
+	}
+	printf("enqueue_msg,n=%d,tid=%ld\n", *((int*)p), pthread_self());
+}
+
 int main() {
+	int err = 0;
+	pthread_t tid_p;
+	pthread_t tid_e;
+	int *num = (int*)malloc(sizeof(int)*20);
+	for (int i = 0; i < 2; i ++) {
+		num[i] = i;
+		err = pthread_create(&tid_p, NULL, thread_process, &num[i]);
+	}
+	for (int i = 0; i < 10; i++) {
+		num[i] = i;
+		err = pthread_create(&tid_e, NULL, thread_enqueue, &num[i]);
+	}
+	pthread_join(tid_p, NULL);
+	pthread_join(tid_e, NULL);
 	return(0);
 }
--- ch15/f15-06.c
+++ ch15/f15-06.c
@@ -3,72 +3,71 @@
 
 #define DEF_PAGER "/bin/more"
 
-int main(int argc, char** argv) {
-	int n;
-	int fd[2];
-	pid_t pid;
-	char *pager, *argv0;
-	char line[MAXLINE];
-	FILE *fp;
+int main(int argc, char **argv) {
+  int n;
+  int fd[2];
+  pid_t pid;
+  char *pager, *argv0;
+  char line[MAXLINE];
+  FILE *fp;
 
-	if (argc != 2) {
-		err_quit("usage : a.out <pathname>");
-	}
-	printf("argv[1]=%s\n", argv[1]); 
-	if ((fp = fopen(argv[1], "r")) == NULL) {
-		err_sys("can’t open %s", argv[1]);
-	}
-	if (pipe(fd) < 0) {
-		err_sys("pipe error");
-	}
+  if (argc != 2) {
+    err_quit("usage : a.out <pathname>");
+  }
+  printf("argv[1]=%s\n", argv[1]);
+  if ((fp = fopen(argv[1], "r")) == NULL) {
+    err_sys("can’t open %s", argv[1]);
+  }
+  if (pipe(fd) < 0) {
+    err_sys("pipe error");
+  }
 
-	if ((pid = fork()) < 0) {
-		err_sys("fork error");
-	} else if (pid > 0) {
-		close(fd[0]); /* close read end */
-		/* parent copies argv[1] to pipe */
-		int i = 0;
-		while (fgets(line, MAXLINE, fp) != NULL) {
-			printf("i = %d, line = %s", i++, line);
-			n = strlen(line);
-			if (write(fd[1], line, n) != n) {
-				err_sys("write error to pipe");
-			}
-		}
-		if (ferror(fp)) {
-			err_sys("fgets error");
-		}
-		close(fd[1]);
-		/* close write end of pipe for reader */
+  if ((pid = fork()) < 0) {
+    err_sys("fork error");
+  } else if (pid > 0) {
+    close(fd[0]); /* close read end */
+    /* parent copies argv[1] to pipe */
+    int i = 0;
+    while (fgets(line, MAXLINE, fp) != NULL) {
+      printf("i = %d, line = %s", i++, line);
+      n = strlen(line);
+      if (write(fd[1], line, n) != n) {
+        err_sys("write error to pipe");
+      }
+    }
+    if (ferror(fp)) {
+      err_sys("fgets error");
+    }
+    close(fd[1]);
+    /* close write end of pipe for reader */
 
-		if (waitpid(pid, NULL, 0) < 0) {
-			err_sys("waitpid error");
-		}
-		exit(0);
-	} else {
-		/* child */
-		close(fd[1]);
-		if (fd[0] != STDIN_FILENO) {
-			if (dup2(fd[0], STDIN_FILENO) != STDIN_FILENO) {
-				err_sys("dup2 error to stdin");
-			}
-			close(fd[0]);
-		}
-		/* get arguments for excel() */
-		if ((pager = getenv("PAGER"))== NULL) {
-			pager = DEF_PAGER;
-		}
-		if ((argv0 = strrchr(pager, '/')) != NULL) {
-			argv0++;
-		} else {
-			argv0 = pager;
-		}
-		printf("pager=%s\n", pager);
-		printf("argv0=%s\n", argv0);
-		if (execl(pager, argv0, (char*)0) < 0) {
-			err_sys("execl error for %s", pager);
-		}
-	}
-	exit(0);
+    if (waitpid(pid, NULL, 0) < 0) {
+      err_sys("waitpid error");
+    }
+    exit(0);
+  } else {
+    /* child */
+    close(fd[1]);
+    if (fd[0] != STDIN_FILENO) {
+      if (dup2(fd[0], STDIN_FILENO) != STDIN_FILENO) {
+        err_sys("dup2 error to stdin");
+      }
+      close(fd[0]);
+    }
+    /* get arguments for excel() */
+    if ((pager = getenv("PAGER")) == NULL) {
+      pager = DEF_PAGER;
+    }
+    if ((argv0 = strrchr(pager, '/')) != NULL) {
+      argv0++;
+    } else {
+      argv0 = pager;
+    }
+    printf("pager=%s\n", pager);
+    printf("argv0=%s\n", argv0);
+    if (execl(pager, argv0, (char *)0) < 0) {
+      err_sys("execl error for %s", pager);
+    }
+  }
+  exit(0);
 }
- 
